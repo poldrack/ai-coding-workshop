6c90ff85f4ef1a827005808fb2d2cbf0eb5d2395
Russell Poldrack <poldrack@gmail.com>
2026-03-01 09:41:14 -0800

    initial add

---
+++ cohensd.py
@@ -0,0 +1,4 @@
+# Stub for Cohen's d calculation
+
+def cohens_d():
+    return None
---
+++ test_cohensd.py
@@ -0,0 +1,7 @@
+"""Tests for cohens_d: Cohen's d for two independent groups."""
+
+from cohensd import cohens_d
+
+def test_stub():
+    """Test that the stub function returns None."""
+    assert cohens_d() is None
